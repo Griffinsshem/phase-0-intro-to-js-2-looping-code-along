59b8a2e3cd31f42756281b96fbedfc91573f0ac6
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-05-28 11:58:00 +0300

    add phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+
+
